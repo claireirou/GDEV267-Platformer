b7f9b135f49204656ecd620bb23dd08afd558ebd
Claire Irou <c.iroudayassamy@gmail.com>
2019-10-03 12:55:29 -0400

    Matt branch merge with Code

--- Assets/JumpMan.cs
+++ Assets/JumpMan.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class JumpMan : MonoBehaviour
 {
-    GameObject Player;
 
     public MoveAndCollide2D movement;
     public float xInput;
@@ -15,19 +14,22 @@ public class JumpMan : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
-
     public int wallJumpDirection = 0;
-    public int jumpCount = 0;
+    public int faceDirection = 0;
+    public int jumpsRemaining = 0;
     public int maxJumpCount = 2;
 
     public bool wallSliding;
     public bool wallSlideLeft;
     public bool wallSlideRight;
 
-
     public bool wallJumping;
     public float wallJumpDuration = 0.5f;
     private float wallJumpT;
+
+    public bool jumpDashing;
+    public float jumpDashDuration = 0.3f;
+    private float jumpDashT;
 
 
 
@@ -39,52 +41,87 @@ public class JumpMan : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        faceDirection = 1;
     }
-
-
     // Test
     // Update is called once per frame
     void Update()
     {
+        // While jump dashing, negate gravity
+        movement.SetYOverride(jumpDashing);
+
         wallSliding = wallSlideLeft = wallSlideRight = false;
 
-        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
         Vector2 moveDirection = Vector2.zero;
+
+        if (playerInput.x < 0)
+        {
+            //Player was last facing left
+            faceDirection = -1;
+        }
+        else if (playerInput.x > 0)
+        {
+            //Player was last facing right
+            faceDirection = 1;
+        }
 
         if (movement.grounded || movement.wallCollision)
         {
-            jumpCount = maxJumpCount;
+            // If we are grounded or next to wall, reset jumpCount
+            jumpsRemaining = maxJumpCount;
         }
 
         if (movement.onWall && playerInput.x > 0)
         {
+            //Wall is to the right of the player
             wallSlideRight = true;
         }
         else if (movement.onWall && playerInput.x < 0)
         {
+            //Wall is to the left of the player
             wallSlideLeft = true;
         }
 
-        if (!wallJumping)
+        if (!wallJumping && !jumpDashing)
         {
+            //Basic move speed with no actions
             moveDirection = playerInput * moveSpeed;
 
         }
         else if (wallJumping)
         {
+            //wallJump away from wall
             moveDirection = Vector2.right * moveSpeed * wallJumpDirection;
             wallJumpT += Time.deltaTime;
 
             if (wallJumpT > wallJumpDuration)
             {
+                //WallJump time has elapsed, stop wallJumping
                 wallJumpT = 0;
                 wallJumping = false;
             }
         }
-
-        if (Input.GetButtonDown("Jump") && ((movement.grounded || movement.onWall) || jumpCount > 0))
+        else if (jumpDashing)
         {
+            // Set y velocity to zero
+            movement.velocity.y = 0;
+            // jumpDash in direction player is facing
+            moveDirection = Vector2.right * moveSpeed * 3 * faceDirection;
+            jumpDashT += Time.deltaTime;
+
+            if (jumpDashT > jumpDashDuration || movement.grounded)
+            {
+                //jumpDash time has elapsed, stop jumpDashing
+                jumpDashT = 0;
+                jumpDashing = false;
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && ((movement.grounded || movement.onWall)
+            || jumpsRemaining > 0))
+        {
+            // Space was pressed and player can still jump
             if (!wallJumping)
             {
                 Jump();
@@ -92,16 +129,46 @@ public class JumpMan : MonoBehaviour
 
             if (movement.wallCollision)
             {
+                // Player is wallJumping, get direction to push off wall
                 wallJumping = true;
                 wallJumpDirection = GetWallJumpDirection();
             }
 
         }
 
+        if (Input.GetAxis("Vertical") < 0 && !movement.grounded && !wallJumping && !jumpDashing)
+        {
+            // Down  or S key was pressed while jumping, ground pound.
+            movement.velocity = new Vector2(0, jumpForce * -3.5f);
+
+            //TODO more interesting things with ground pound.
+        }
+
+        if ((movement.grounded || wallJumping) && Input.GetButtonDown("Fire3"))
+        {
+            // left shift is being pressed, start sprinting
+            moveSpeed *= 2;
+        }
+        else if (!movement.grounded && !wallJumping && Input.GetButtonDown("Fire3"))
+        {
+            // player is jumping and has pressed left shift
+            jumpDashing = true;
+        }
+
+        if (Input.GetButtonUp("Fire3"))
+        {
+            // Player is not sprinting
+            moveSpeed = 7;
+        }
+
         movement.SetVelocity(moveDirection);  //Velocity equals moveDirection
 
     }
 
+    /**
+    GetWallJumpDirection determines what direction to push off wall
+    @returns -1 to push towards right and 1 to push towards left
+     */
     public int GetWallJumpDirection()
     {
         int direction = 1;
@@ -115,25 +182,31 @@ public class JumpMan : MonoBehaviour
             direction = -1;
         }
 
-        Debug.Log(direction);
+        //Debug.Log(direction);
         return direction;
     }
 
-    
-
+    /**
+    Jump method handles calculating the jumping velocity and decreasing 
+    the jumpsRemaining.
+    */
     private void Jump()
     {
         movement.velocity = new Vector2(movement.velocity.x, jumpForce);
-        jumpCount--;
+        jumpsRemaining--;
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
 
-        Debug.Log("Hit");
+        //Debug.Log("Hit");
         if (other.gameObject.CompareTag("Coin"))
         {
-            Debug.Log("Is coin");
+            //Debug.Log("Is coin");
             other.gameObject.SetActive(false);
         }
     }
 }
+
+
+
